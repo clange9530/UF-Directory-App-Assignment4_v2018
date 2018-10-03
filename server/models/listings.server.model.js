c6befb19fd0452fe05cf22d647153f4071eea92b
clange9530 <clange9530@gmail.com>
2018-10-03 07:27:21 -0400

    Update listings.server.model.js

--- server/models/listings.server.model.js
+++ server/models/listings.server.model.js
@@ -1,17 +1,36 @@
-//You can replace this entire file with your Bootcamp Assignment #3 - ListingSchema.js File
-
-/* Import mongoose and define any variables needed to create the schema */
 var mongoose = require('mongoose'), 
     Schema = mongoose.Schema;
 
 /* Create your schema */
 var listingSchema = new Schema({
-  /* your code here from Bootcamp Assignment #3 - ListingSchema.js File*/
-});
+  code: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  coordinates: {
+    latitude: Number,
+    longitude: Number
+  },
+  address: String
+
+},
+{
+  timestamps: {createdAt: 'created_at', updatedAt: 'updated_at'}
+}
+);
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
 listingSchema.pre('save', function(next) {
-  /* your code here from Bootcamp Assignment #3 - ListingSchema.js File */
+  now = new Date();
+  this.updated_at = now;
+  if(!this.created_at){
+    this.created_at = now;
+  }
+  next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
